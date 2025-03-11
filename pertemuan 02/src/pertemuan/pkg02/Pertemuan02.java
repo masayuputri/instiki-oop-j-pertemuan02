3d2cf606dc25d091cf81a349a4c140ec28f47822
masayu putri <masayuputri275@gmail.com>
2025-03-11 21:58:33 +0800

    first commit

---
+++ pertemuan 02/src/pertemuan/pkg02/Pertemuan02.java
@@ -0,0 +1,24 @@
+package pertemuan.pkg02;
+
+/**
+ *
+ * @author masayu
+ * tgl: 11  Maret 2025
+ */
+public class Pertemuan02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      String nama = "Masayu";  
+      String nim ="2301010443" ;
+      
+      System.out.println("Pertemuan 2 Java");
+      System.out.println("~~~~~~~~~~~~~~~~~~");
+      System.out.println("nama : "+nama) ;
+      System.out.println("nim  : "+nim);
+      System.out.printf("nama saya %s dengan NIM %s, saat ini sedang belajar menjadi Leader yang baik",nama,nim);
+    }
+    
+}
